e8c432be39e9177ae5831c6edb3f519a21895c6c
nivedhithaa-coder <nivedhithaa.2018@gmail.com>
2025-09-04 16:35:59 +0530

    admin

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -89,8 +89,8 @@ export const createBooking=async(req,res)=>{
         }]
 
         const session=await stripeInstance.checkout.sessions.create({
-            success_url: `${origin}/loading/my-bookings`,
-            cancel_url:`${origin}/my-bookings`,
+            success_url: `${origin}/loading/movies`,
+            cancel_url:`${origin}/movies`,
             line_items:line_items,
             mode:'payment',
             metadata:{
